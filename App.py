a7815fef2ee611cb1ccd6b08b7f28b6451816339
lura168 <kaxn20003@gmail.com>
2024-07-20 10:17:29 +0700

    first commit

---
+++ App.py
@@ -0,0 +1,27 @@
+# print("Hello kitty"+" From bbu"+str(6))
+# print('double qoute')
+# print("single \'quote\' and \"double\" qoute \ninside qoute")
+
+# # name=input("whate is your name?")
+# # print("sok " + name)
+# qty1=int(input("Qty1 = "))
+# qty2=int(input("Qty2 = "))
+# price=float(input("Price = "))
+# total_qty=qty1+qty2
+# print("Total Qty= "+str(total_qty))
+# amount=total_qty*price
+# print("Total Amount="+str(amount))
+
+# a = input("input a =")
+# b = input("input b =")
+# c = a
+# a = b
+# b = c
+# print("a:" + a)
+# print("b:" + b)
+
+print("welcome to the band name generator")
+city=input("which city did you grow up in?\n")
+pet=input("What is your pet name ?\n")
+
+print("your band name most be " + city +" " +pet)
